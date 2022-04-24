acdc8c3c706ad69188e01d384cc82382c87549d3
LegosAndStuff-Dev <masonnorberg@midco.net>
2022-04-24 16:37:40 -0500

    Removed un-needed commands

--- cogs/utility.py
+++ cogs/utility.py
@@ -16,31 +16,6 @@ from functions.version import version
 class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    
-    #Feedback Command -feedback
-    @commands.command()
-    async def feedback(self, ctx, *, message=None):
-        feedback = self.bot.get_channel(840422064975249418)
-        user_id = ctx.message.author.id
-        user_name = ctx.message.author
-
-        if message == None:
-            await ctx.send("Please provide some feedback that we can improve on!")
-        
-        else:
-            embed: discord.Embed = discord.Embed(
-                title="Feedback",
-                description=f"<@!{user_id}> gave some feedback!",
-                color=discord.Color.green()
-            )
-            embed.add_field(name="User Name:", value=f"{user_name}")
-            embed.add_field(name="User ID:", value=f"{user_id}")
-            embed.add_field(name="Feedback:", value=message, inline=False)
-
-            await feedback.send(embed=embed)
-
-            await ctx.send("Feedback Was Submitted!")
 
     #Bot Command -bot
     @commands.command()
@@ -75,31 +50,6 @@ class Utility(commands.Cog):
         )
         
         await ctx.send(embed=embed)
-
-
-    #Bug Command -bug
-    @commands.command()
-    async def bug(self, ctx, *, message=None):
-        bug = self.bot.get_channel(842160990196203520)
-        user_id = ctx.message.author.id
-        user_name = ctx.message.author
-
-        if message == None:
-            await ctx.send("Please report the bug after you do the command.")
-
-        else:
-            embed: discord.Embed = discord.Embed(
-                title="Bug Report",
-                description=f"<@!{user_id}> Has reported a bug!!",
-                color=discord.Color.green() 
-            )
-            embed.add_field(name="User Name:", value=f"{user_name}")
-            embed.add_field(name="User ID:", value=f"{user_id}")
-            embed.add_field(name="Bug:", value=message, inline=False)
-
-            await bug.send(embed=embed)
-
-            await ctx.send("The bug was reported!")
     
 
     #Servers Command -servers
@@ -108,13 +58,6 @@ class Utility(commands.Cog):
         servers = len(self.bot.guilds)
 
         await ctx.send(f"I'm in ``{servers}`` servers!")
-
-    
-    #Support Command -support
-    @commands.command()
-    async def support(self, ctx):
-        await ctx.send("If you ever need support with Dinosaur join with the link bellow\nhttps://discord.gg/KxPuFvazuF")
-
 
     #Uptime Command -uptime
     @commands.command()

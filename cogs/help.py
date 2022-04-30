344e2ca5f5d14c417594304eec1b20e5dce44415
LegosAndStuff-Dev <masonnorberg@midco.net>
2022-04-30 13:59:14 -0500

    added ice cream sub category

--- cogs/help.py
+++ cogs/help.py
@@ -7,20 +7,30 @@ class Help(commands.Cog):
 
     #Help Command
     @commands.command()
-    async def help(self, ctx, arg1 = None):
+    async def help(self, ctx, *, arg1 = None):
         if arg1 == None:
             embed: discord.Embed = discord.Embed(
                 title="**Help**",
                 color=discord.Color.green()
             )
-            embed.add_field(name="Make", value="Make some ice cream with this command `ice make`", inline=False)
-            embed.add_field(name="Day", value="""Have a "day" of work with this command `ice day`""", inline=False)
+            embed.add_field(name="Ice Cream", value="Want to see all the ice cream related commands do `ice help ice cream`")
             embed.add_field(name="Economy", value="Want to see all the economy commands do `ice help economy`", inline=False)
             embed.add_field(name="Utility", value="Want to see all the utility commands do `ice help utility`", inline=False)
 
             await ctx.send(embed=embed)
 
-        if arg1.lower() == "economy":
+        if arg1.lower() == "ice cream":
+            embed: discord.Embed = discord.Embed(
+                title="Ice Cream Help"
+            )
+            embed.add_field(name="Make", value="Make some ice cream with this command `ice make`", inline=False)
+            embed.add_field(name="Day", value="""Have a "day" of work with this command `ice day`""", inline=False)
+            embed.add_field(name="Advertise", value="See how many advertisements you have do `ice advertise`\n > Buy an ad by doing `ice advertise buy`\n > Use an ad by doing `ice advertise use`", inline=False)
+            embed.add_field(name="Location", value="Want to see how many locations you own do `ice location`\n > Want to buy another location do `ice location buy`", inline=False)
+            
+            await ctx.send(embed=embed)
+
+        elif arg1.lower() == "economy":
             embed: discord.Embed = discord.Embed(
                 title="Economy Help",
                 color=discord.Color.green()
